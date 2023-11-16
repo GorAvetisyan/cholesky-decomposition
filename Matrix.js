722cc3279aebd114ab93b22c712c8844e2742bc6
GorAvetisyan <gor03avetisyan@gmail.com>
2023-11-16 12:36:19 +0400

    Matrix class skeleton added

---
+++ Matrix.js
@@ -0,0 +1,44 @@
+class Matrix{
+    
+    // are n, m required for contructor ?
+
+    constructor(matrix){
+
+        // input matrix validation
+
+        this.matrix = matrix;
+    }
+
+    isSquare(){
+
+    }
+
+    isSymmetric(){
+
+    }
+
+    isEqualTo(){
+
+    }
+
+    transpose(){
+
+    }
+
+    determinant(){
+
+    }
+
+    type(){
+
+    }
+
+    print(){
+
+    }
+
+}
+
+
+const A = new Matrix([[1, 0], [0, 1]]);
+
