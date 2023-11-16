aeefc762d3e3889b26c8d1388828aac02adc09d0
nerses-asaturyan <nerses.asaturyan@gmail.com>
2023-11-16 20:43:39 +0400

    test matrices added

---
+++ test.js
@@ -0,0 +1,45 @@
+const Matrix = require("./Matrix.js");
+
+const matrix1 = new Matrix([
+  [0, 0, 2],
+  [5, 9, 9],
+  [9, 9, 6],
+]);
+
+const matrix2 = new Matrix([
+  [5, 7, 7],
+  [1, 3, 2],
+  [3, 0, 8],
+]);
+
+const matrix3 = new Matrix([
+  [5, 8, 4, 6],
+  [2, 7, 7, 7],
+  [8, 4, 2, 3],
+  [4, 6, 0, 8],
+]);
+
+const matrix4 = new Matrix([
+  [3, 7, 7, 1],
+  [3, 3, 8, 5],
+  [9, 7, 6, 1],
+  [5, 4, 5, 3],
+]);
+
+const matrix5 = new Matrix([
+  [1, 1, 5, 8, 8],
+  [3, 5, 6, 3, 8],
+  [7, 6, 7, 6, 1],
+  [2, 0, 8, 8, 5],
+  [2, 8, 9, 7, 5],
+]);
+
+const matrix6 = new Matrix([
+  [5, 5, 3, 3, 3],
+  [7, 3, 4, 1, 0],
+  [0, 0, 6, 5, 3],
+  [1, 5, 7, 2, 3],
+  [3, 8, 9, 0, 8],
+]);
+
+console.log(matrix1.determinant());
