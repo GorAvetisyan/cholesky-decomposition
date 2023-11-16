d1d0da985813ca7068ae0532de3aeb295b99d837
GorAvetisyan <gor03avetisyan@gmail.com>
2023-11-16 18:13:19 +0400

    matrix validation added in constructor

--- Matrix.js
+++ Matrix.js
@@ -4,13 +4,38 @@ class Matrix{
 
     constructor(matrix){
 
-        // input matrix validation
+        if(Array.isArray(matrix)){
 
-        this.matrix = matrix;
+            let n = matrix.length;
+            let m;
+
+            for (let i = 0; i < n; i++) {
+                if(Array.isArray(matrix[i])){
+                    if(m == undefined){
+                        m = matrix[i].length;
+                    }else{
+                        if(matrix[i].length == m){
+                            for (let j = 0; j < m; j++) {
+                                if(typeof matrix[i][j] !== 'number'){
+                                    throw Error(`Matrix elements must be numbers: matrix[${i+1}][${j+1}] is ${typeof matrix[i][j]}`);
+                                }
+                            }
+                        }else{
+                            throw Error(`${i+1}th row need to have ${m} elements`);
+                        }
+                    }
+                }else{
+                    throw Error(`${i+1}th row must be an array, got an ${typeof matrix[i]}`);
+                }
     }
 
-    isSquare(){
+            this.n = n;
+            this.m = m;
+            this.matrix = matrix;
 
+        }else{
+            throw Error(`Matrix must be an array, got a ${typeof matrix}`);
+        }
     }
 
     isSymmetric(){
