272af14ec863182fe6885c8cea071db31ec8bd1b
GorAvetisyan <gor03avetisyan@gmail.com>
2023-11-20 12:56:59 +0400

    add: isNumber function and tests added

--- helpers.js
+++ helpers.js
@@ -2,4 +2,10 @@ const isNatural = n => {
   return typeof n === "number" && !isNaN(n) && n > 0 && n === Math.floor(n);
 };
 
-module.exports = { isNatural };
+// Number class ???
+
+const isNumber = n => {
+  return typeof n === "number" && !isNaN(n);
+};
+
+module.exports = { isNatural, isNumber };
--- tests/helpers.test.js
+++ tests/helpers.test.js
@@ -1,4 +1,4 @@
-const { isNatural } = require("../helpers");
+const { isNatural, isNumber } = require("../helpers");
 
 test("1", () => {
   expect(isNatural(15)).toBe(true);
@@ -22,4 +22,51 @@ test("5", () => {
 
 test("6", () => {
   expect(isNatural(NaN)).toBe(false);
+});
+
+describe("isNumber function", () => {
+  it("should return true for a valid positive number", () => {
+    const result = isNumber(42);
+    expect(result).toBe(true);
+  });
+
+  it("should return true for a valid negative number", () => {
+    const result = isNumber(-3.14);
+    expect(result).toBe(true);
+  });
+
+  it("should return true for zero", () => {
+    const result = isNumber(0);
+    expect(result).toBe(true);
+  });
+
+  it("should return false for a non-numeric string", () => {
+    const result = isNumber("not a number");
+    expect(result).toBe(false);
+  });
+
+  it("should return false for an array", () => {
+    const result = isNumber([1, 2, 3]);
+    expect(result).toBe(false);
+  });
+
+  it("should return false for an object", () => {
+    const result = isNumber({ key: "value" });
+    expect(result).toBe(false);
+  });
+
+  it("should return false for null", () => {
+    const result = isNumber(null);
+    expect(result).toBe(false);
+  });
+
+  it("should return false for undefined", () => {
+    const result = isNumber(undefined);
+    expect(result).toBe(false);
+  });
+
+  it("should return false for NaN", () => {
+    const result = isNumber(NaN);
+    expect(result).toBe(false);
+  });
 });
